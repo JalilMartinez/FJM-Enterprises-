b28717c755221491e75e339094e3dceee6008064
Mazatl <jalilmazatl@gmail.com>
2024-03-28 22:51:56 -0600

    contenedores about us

--- T-Ocean/T-OceanFront/src/components/principalComponents/LoginComponent/LoginComponent.tsx
+++ T-Ocean/T-OceanFront/src/components/principalComponents/LoginComponent/LoginComponent.tsx
@@ -5,7 +5,7 @@ const LoginComponent = () => {
             <h2>Hola desde LoginComponent</h2>
             <div className="login-image">
                 Aqui va ir la imagen del login
-
+            </div>
             <div className="formContainer">
 
 
