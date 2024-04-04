f7913d70421e215a9fa88dc0751e55a06fc0b70d
Mazatl <jalilmazatl@gmail.com>
2024-04-04 09:46:11 -0600

    Correccion componente login

--- T-Ocean/T-OceanFront/src/components/principalComponents/LoginComponent/LoginComponent.tsx
+++ T-Ocean/T-OceanFront/src/components/principalComponents/LoginComponent/LoginComponent.tsx
@@ -1,11 +1,11 @@
 const LoginComponent = () => {
-    return (
+    return(
         <>
 
             <h2>Hola desde LoginComponent</h2>
             <div className="login-image">
                 Aqui va ir la imagen del login
-
+            </div>
             <div className="formContainer">
 
 
